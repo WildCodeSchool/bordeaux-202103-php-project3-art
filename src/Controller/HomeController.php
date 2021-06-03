2cae2b4a1dcc94d502ccb172766d3c3a012e20c3
Nathalie-Sansone <nathalie.sansone@gmail.com>
2021-06-03 11:53:48 +0200

    add contact form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Happening;
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,6 +21,30 @@ class HomeController extends AbstractController
         $users = $userRepository->findAll();
         return $this->render('home/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function sendMessage(Request $request): Response
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $message->setSendAt(new \DateTime());
+            $message->setIsRead(false);
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Message envoyé !');
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
