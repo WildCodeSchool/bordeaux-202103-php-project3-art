28900a9e106df0632578feeaec3a3e2c2c3c9d62
A-david1 <antoine.david.pro1@gmail.com>
2021-07-07 11:21:24 +0200

    remove EasyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
