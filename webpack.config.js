4d7c15f9bbcf0fda15656ce008ff970c9320d122
Nathalie-Sansone <nathalie.sansone@gmail.com>
2021-07-07 17:01:51 +0200

    add dashboard layout

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,6 @@ Encore
     .addEntry('inputFileValue', './assets/inputFileValue.js')
     .addEntry('friend', './assets/friend.js')
     .addEntry('location', './assets/preciseLocation.js')
-
-
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
