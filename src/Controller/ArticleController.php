619d03c2abac0ef69f38f1d6daba951a6d58aaee
Nathalie-Sansone <nathalie.sansone@gmail.com>
2021-07-28 11:23:10 +0200

    change display single article + discover other portraits

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,18 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    public const MAX_ARTICLES_DISPLAY = 3;
+
     /**
      * @Route("/article/{id}", name="article")
      */
-    public function index(Article $article): Response
+    public function index(Article $article, ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findBy(
+            [],
+            ['id' => 'DESC'],
+            self::MAX_ARTICLES_DISPLAY,
+        );
+
         return $this->render('article/index.html.twig', [
+            'articles' => $articles,
             'article' => $article,
         ]);
     }
